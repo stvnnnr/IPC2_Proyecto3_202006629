b392054dfb7605fb0fc91af1f2e11ef4968b2de6
steven ruano <3605181090101@ingenieria.usac.edu.gt>
2022-05-05 11:38:25 -0600

    Solo falta documentación

--- Backend/app.py
+++ Backend/app.py
@@ -77,9 +77,20 @@ def generexPDF():
 @app.route('/probarMensaje', methods=['POST'])
 def pruebaM():
     if request.method == 'POST':
-        letras = json.loads(request.data)
-        print(letras)
-        return jsonify({"status": 200})
+        mensae = request.data.decode('utf-8')
+        vari = msjUno(mensae)
+        return jsonify({"vari": vari})
+    
+#Metodo clasi por fecha y empresa
+@app.route('/clasiEmpresa', methods=['POST'])
+def clasiEmpresa():
+    if request.method == 'POST':
+        mensae = request.data.decode('utf-8')
+        y = json.loads(mensae)
+        fecha = y["fecha"]
+        empresa = y["empresa"]
+        vari = clasiporEmpresa(fecha,empresa)
+        return jsonify({"vari": vari})
 
 #Metodos propios en python para procesar todos los archivos de manera interna
 def msj(letras):
@@ -187,16 +198,22 @@ def FuncionDos(msj):
     contNegativas = 0
     contPositivas = 0
     for posi in Positivas:
-        posi = castear(posi)
-        msj = castear(msj)
-        if posi in msj:
-            contPositivas = contPositivas + 1
+        posiUno = castear(posi)
+        msjUno = castear(msj)
+        msjDos = msjUno.split(" ")
+        for ss in msjDos:
+            if posiUno in ss:
+                print(posiUno)
+                contPositivas = contPositivas + 1
     
     for nega in Negativas:
         nega = castear(nega)
         msj = castear(msj)
-        if nega in msj:
-            contNegativas = contNegativas + 1
+        msjTres = msj.split(" ")
+        for rr in msjTres:
+            if nega == rr:
+                print(nega)
+                contNegativas = contNegativas + 1
     
     sumPal = (contPositivas+contNegativas)
     porPosiNum = ((contPositivas*100)/sumPal)
@@ -211,6 +228,87 @@ def FuncionDos(msj):
         return ("Negativo",contPositivas,contNegativas,porPosi,porNega)
     else:
         return ("Neutro",contPositivas,contNegativas,porPosi,porNega)
+
+def msjUno(letras):
+    global Positivas
+    global Negativas
+    global listaEmpresas
+    global fechas
+
+    documentt = parseString(str(letras))
+    mensajes = documentt.getElementsByTagName("mensaje")
+    
+    for x in mensajes:
+        textoCrudo = str(x.childNodes[0].nodeValue)
+        mensajeCrudo = textoCrudo.strip()#
+        crudUno = mensajeCrudo.split("Lugar y fecha:")[1]
+        lugar = crudUno.split(",")[0].strip()#
+        crudDos = crudUno.split(",")[1].strip()
+        crudoDos = crudDos.split()
+        fecha = crudoDos[0]#
+        hora = crudoDos[1]#
+        resto = crudDos.split("Usuario:")[1].strip()
+        usuario = resto.split()[0]#
+        restoDos = resto.split("Red social:")[1].strip()
+        redSocial = restoDos.split()[0]#
+        restoTres = crudUno.split(redSocial)[1].strip()
+        msjSucio = restoTres.replace("\n"," ")
+        msjSucio = msjSucio.replace("\t","")
+        msjSucio = msjSucio.replace("  "," ")
+        msjLimpio = msjSucio.replace("  "," ")#
+        msjLimpio = re.sub(r"""[:!?'".<>(){}@%&*/[/]""", " ", msjLimpio)
+        msj = mensajito(lugar,fecha,hora,usuario,redSocial,msjLimpio)
+            
+        for empre in listaEmpresas:
+            if empre.nombre in msjLimpio:
+                msj.setEmpresa(empre.nombre)
+        
+        sentimiento = FuncionDos(msjLimpio)
+        msj.setSentimiento(sentimiento[0])
+        msj.setPositivas(sentimiento[1])
+        msj.setNegativas(sentimiento[2])
+        msj.setPorcePositivas(sentimiento[3])
+        msj.setPorceNegativas(sentimiento[4])
+        vari = procesarUno(msj)
+
+        return vari
+
+def procesarUno(msj):
+    global listaEmpresas
+
+    var = ""
+    var  = var + f'''<respuesta><fecha>{msj.fecha}</fecha>'''
+    var  = var + f'''<red_social>{msj.redSocial}</red_social>'''
+    var  = var + f'''<usuario>{msj.usuario}</usuario><empresas>'''
+    for emprex in listaEmpresas:
+            varDos = ""
+            empresitas = msj.getEmpresa()
+            if emprex.nombre in empresitas:
+                servix = emprex.getServicios()
+                varTres = ""
+                for serv in servix:
+                    listaAlias = serv.getAlias()
+                    nombreServi = serv.nombre
+                    for alix in listaAlias:
+                        alixUno = (alix)
+                        mms = (msj.msjLimpio)
+                        if castear(alixUno) in castear(mms):
+                            varTres = varTres + f'''<servicio>{nombreServi}</servicio>'''
+                            break
+                    if castear(nombreServi) in castear(mms):
+                        varTres = varTres + f'''<servicio>{nombreServi}</servicio>'''
+                varDos = varDos + f'''<empresa nombre={emprex.nombre}>'''
+                varDos = varDos + varTres
+                varDos = varDos +f'''</empresa>'''
+    var = var + varDos
+    var = var + f'''</empresas>'''
+    var = var + f'''<palabras_positivas>{msj.getPositivas()}</palabras_positivas>'''
+    var = var + f'''<palabras_negativas>{msj.getNegativas()}</palabras_negativas>'''
+    var = var + f'''<sentimiento_positivo>{msj.getPorcePositivas()}</sentimiento_positivo>'''
+    var = var + f'''<sentimiento_negativo>{msj.getPorceNegativas()}</sentimiento_negativo>'''
+    var = var + f'''<sentimiento_analizado>{msj.getSentimiento()}</sentimiento_analizado>'''
+    var = var + f'''</respuesta>'''
+    return var
 
 def procesar():
     global Positivas
@@ -309,18 +407,18 @@ def procesar():
     return "Yep"
 
 def castear(texto):
-    texto.replace("á","a")
-    texto.replace("é","e")
-    texto.replace("í","i")
-    texto.replace("ó","o")
-    texto.replace("ú","u")
-    texto.replace("Á","A")
-    texto.replace("É","E")
-    texto.replace("Í","I")
-    texto.replace("Ó","O")
-    texto.replace("Ú","U")
-    texto.lower()
-    return texto
+    textoUno = texto.replace("á","a")
+    textoDos = textoUno.replace("é","e")
+    textoTres = textoDos.replace("í","i")
+    textoCuatro = textoTres.replace("ó","o")
+    textoCinco = textoCuatro.replace("ú","u")
+    textoA = textoCinco.replace("Á","A")
+    textoB = textoA.replace("É","E")
+    textoC = textoB.replace("Í","I")
+    textoD = textoC.replace("Ó","O")
+    textoE = textoD.replace("Ú","U")
+    textoZ = textoE.lower()
+    return textoZ
 
 def geneTXT(texto,textoDos):
     f = open ('db.txt','a')
@@ -346,6 +444,158 @@ def genePDF():
     pdf.output("REPORTE.pdf")
     fichero.close()
     return "Yep"
+
+def clasiporEmpresa(fecha, empresa):
+    global Positivas
+    global Negativas
+    global listaEmpresas
+    global listaMensaje
+    global fechas
+    var = '''<?xml version=\"1.0\"?><lista_respuestas>'''
+    positivs = 0
+    negativs = 0
+    neutros = 0
+    nom = 0
+    var  = var + f'''<respuesta><fecha>{fecha}</fecha><mensajes>'''
+
+    for mensa in listaMensaje:
+        if str(mensa.fecha) == str(fecha):
+            nom = nom+1
+            if mensa.getSentimiento() == "Positivo":
+                positivs = positivs+1
+            elif mensa.getSentimiento() == "Negativo":
+                negativs = negativs+1
+            else:
+                neutros = neutros+1
+            
+    var = var + f'''<total>{nom}</total><positivos>{positivs}</positivos><negativos>{negativs}</negativos><neutros>{neutros}</neutros></mensajes>'''
+    var = var +f'''<analisis>'''
+    
+    if str(empresa) == "todas":
+        for emprex in listaEmpresas:
+            varDos = ""
+            toti = 0
+            posi = 0
+            negi = 0
+            neutri = 0
+            for men in listaMensaje:
+                if str(men.fecha) == str(fecha):
+                    empresitas = men.getEmpresa()
+                    if emprex.nombre in empresitas:
+                        toti = toti + 1
+                        if men.getSentimiento() == "Positivo":
+                            posi = posi+1
+                        elif men.getSentimiento() == "Negativo":
+                            negi = negi+1
+                        else:
+                            neutri = neutri+1
+            servix = emprex.getServicios()
+            varTres = ""
+            for serv in servix:
+                totix = 0
+                posix = 0
+                negix = 0
+                neutrix = 0
+                listaAlias = serv.getAlias()
+                nameUno = castear(serv.nombre)
+                name = serv.nombre
+                for men in listaMensaje:
+                    if str(men.fecha) == str(fecha):
+                        msj = castear(men.msjLimpio)
+                        if nameUno in msj:
+                            totix = totix + 1
+                            if men.getSentimiento() == "Positivo":
+                                posix = posix+1
+                            elif men.getSentimiento() == "Negativo":
+                                negix = negix+1
+                            else:
+                                neutri = neutri+1
+                        for alix in listaAlias:
+                            alixUno = castear(alix)
+                            if alixUno in msj:
+                                totix = totix + 1
+                                if men.getSentimiento() == "Positivo":
+                                    posix = posix+1
+                                elif men.getSentimiento() == "Negativo":
+                                    negix = negix+1
+                                else:
+                                    neutrix = neutrix+1
+                varTres = varTres + f'''<servicio nombre="{name}"><mensajes>'''
+                varTres = varTres + f'''<total>{totix}</total><positivos>{posix}</positivos><negativos>{negix}</negativos><neutros>{neutrix}</neutros></mensajes></servicio>'''
+
+
+            varDos = varDos + f'''\t\t\t\t<empresa nombre="{emprex.nombre}">\n\t\t\t\t\t<mensajes>\n'''
+            varDos = varDos +f'''<total>{toti}</total><positivos>{posi}</positivos><negativos>{negi}</negativos><neutros>{neutri}</neutros></mensajes>'''
+            varDos = varDos +f'''<servicios>'''
+            varDos = varDos + varTres
+            varDos = varDos +f'''</servicios></empresa>'''
+            var = var + varDos
+    else:
+        for emmm in listaEmpresas:
+            if str(empresa) == emmm.nombre:
+                varDos = ""
+                toti = 0
+                posi = 0
+                negi = 0
+                neutri = 0
+                for men in listaMensaje:
+                    if str(men.fecha) == str(fecha):
+                        empresitas = men.getEmpresa()
+                        if empresa in empresitas:
+                            toti = toti + 1
+                            if men.getSentimiento() == "Positivo":
+                                posi = posi+1
+                            elif men.getSentimiento() == "Negativo":
+                                negi = negi+1
+                            else:
+                                neutri = neutri+1
+                for em in listaEmpresas:
+                    varTres = ""
+                    if castear(empresa) == castear(str(em.nombre)):
+                        servixx = em.getServicios()
+                        for serv in servixx:
+                            totix = 0
+                            posix = 0
+                            negix = 0
+                            neutrix = 0
+                            listaAlias = serv.getAlias()
+                            nameUno = castear(serv.nombre)
+                            name = serv.nombre
+                            for men in listaMensaje:
+                                if str(men.fecha) == str(fecha):
+                                    msj = castear(men.msjLimpio)
+                                    if nameUno in msj:
+                                        totix = totix + 1
+                                        if men.getSentimiento() == "Positivo":
+                                            posix = posix+1
+                                        elif men.getSentimiento() == "Negativo":
+                                            negix = negix+1
+                                        else:
+                                            neutri = neutri+1
+                                    for alix in listaAlias:
+                                        alixUno = castear(alix)
+                                        if alixUno in msj:
+                                            totix = totix + 1
+                                            if men.getSentimiento() == "Positivo":
+                                                posix = posix+1
+                                            elif men.getSentimiento() == "Negativo":
+                                                negix = negix+1
+                                            else:
+                                                neutrix = neutrix+1
+                            varTres = varTres + f'''<servicio nombre="{name}"><mensajes>'''
+                            varTres = varTres + f'''<total>{totix}</total><positivos>{posix}</positivos><negativos>{negix}</negativos><neutros>{neutrix}</neutros></mensajes></servicio>'''
+
+
+                varDos = varDos + f'''<empresa nombre="{em.nombre}"><mensajes>'''
+                varDos = varDos +f'''<total>{toti}</total><positivos>{posi}</positivos><negativos>{negi}</negativos><neutros>{neutri}</neutros></mensajes>'''
+                varDos = varDos +f'''<servicios>'''
+                varDos = varDos + varTres
+                varDos = varDos +f'''</servicios></empresa>'''
+                var = var + varDos
+    var = var + f'''</analisis></respuesta>'''
+    var = var + f'''</lista_respuestas>'''
+    geneTXT("La misma entrada",var)
+    return var
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
